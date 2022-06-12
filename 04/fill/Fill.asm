a42054269e824a50c1ba63bdad1ea6a5074cd0d0
Yuta Tezuka <yuta.tzk@gmail.com>
2022-06-12 21:32:17 +0900

    Fill.asmを途中まで実装（動かず）

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// （注意）動いてない
+
+(Loop)
+    @KBD // キーボードのベースアドレス
+    D=M
+    @White
+    D; JEQ
+    @Black
+    0; JMP
+
+(White)
+    @0
+    D=A
+    @color
+    M=D
+    @SetColor
+    0; JMP
+
+(Black)
+    //@65535 // 黒
+    //D=A
+    //@color
+    M=-1
+    @SetColor
+    0; JMP
+
+(SetColor)
+    @i
+    M=0
+(SetColorLoop)
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @num_word
+    M=D
+    @color
+    D=M
+    @num_word
+    M=D
+
+    @i
+    M=M+1
+    @8192
+    D=A // 画面のワード数
+    @num_word
+    D=D-M
+    @SetColorLoop
+    D; JGT
+
+    @Loop
+    0; JMP
