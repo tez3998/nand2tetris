3843e46adae2086f132b1b27690225381a0be085
Yuta Tezuka <yuta.tzk@gmail.com>
2022-06-17 06:17:43 +0900

    Improve readability

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -12,29 +12,44 @@
 // Put your code here.
 
     // R2=0
-    @2
+    @R2
+    M=0
+
+    // i=R1
+    @R1
+    D=M
+    @i
+    M=D
+
+    // sum=0
+    @sum
     M=0
 
 (LOOP)
-    // R0; JEQ
-    @0
+    // i; JEQ
+    @i
     D=M
     @END
     D; JEQ
 
-    // R2=R2+R2
-    @1
+    // sum=sum+R0
+    @R0
     D=M
-    @2
+    @sum
     M=D+M
 
-    // R0=R0-1
-    @0
+    // i=i-1
+    @i
     M=M-1
+
+    // R2=sum
+    @sum
+    D=M
+    @R2
+    M=D
 
     @LOOP
     0; JMP
-
 (END)
     @END
     0; JMP
