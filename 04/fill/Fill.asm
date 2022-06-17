481199550726a59e4c9883ef76cedfe0530691c9
Yuta Tezuka <yuta.tzk@gmail.com>
2022-06-17 17:54:23 +0900

    Fill.asmを実装
    当初、全ての画面の色を変えることを試みたが、クロックが遅すぎるので1ワードだけ変更するように仕様を変えた。

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -13,74 +13,77 @@
 
 // Put your code here.
 
-// （注意）動いてない
+(INIT) 
+    @8191 // 画面のワード数-1。-1はアドレスが0からスタートするため。 ((512 * 256) / 16) - 1
+    D=A
+    @SCREEN
+    D=D+A
+    @max_address // スクリーンのアドレスの最大値
+    M=D
 
-(MainLoop)
+    @SCREEN
+    D=A
+    @current_address // 現在の色を変更しようと思っているスクリーンのアドレス
+    M=D
+
+(MAIN)
     @KBD
     D=M
-    //@Whiten
-    //D; JEQ
-    @Blaken
+    @kbd_status // キーボードの入力
+    M=D
+    @WHITEN
+    D; JEQ
+    @BLAKEN
     0; JMP
 
-(Whiten)
-    @8192 // (512px x 256px)/16bit。スクリーン中のワードの数
-    D=A
-    @i
-    M=D
+(LOOP1)
+    @kbd_status
+    D=M
+    @DEC
+    D; JEQ
+    @INC
+    0; JMP
 
+(LOOP2)
+    @MAIN
+    0; JMP
+
+(WHITEN) // アドレスcurrent_addressに対応するピクセルを白くする
+    @current_address
+    D=M
+    A=D
+    M=0
+    @LOOP1
+    0; JMP
+
+(BLAKEN) // アドレスcurrent_addressに対応するピクセルを黒くする
+    @current_address
+    D=M
+    A=D
+    M=-1
+    @LOOP1
+    0; JMP
+
+(DEC) // current_addressを減少させる
     @SCREEN
     D=A
     @current_address
-    M=D
-
-(WhitenLoop)
-    @i
-    D=M
-    @MainLoop
-    D; JEQ
-
+    D=M-D
+    @LOOP2
+    D; JLT
     @current_address
-    M=0
+    M=M-1
+    @LOOP2
+    0; JMP
 
+(INC) // current_addressを増加させる
+    @max_address
+    D=M
+    @current_address
+    D=D-M
+    @LOOP2
+    D; JLT
     @current_address
     M=M+1
-
-    @i
-    M=M-1
-
-    @WhitenLoop
-    0; JMP
-
-(Blaken)
-    @8192 // (512px x 256px)/16bit。スクリーン中のワードの数
-    D=A
-    @i
-    M=D
-
-    @SCREEN
-    D=A
-    @current_address
-    M=D
-
-(BlakenLoop)
-    @i
-    D=M
-    @MainLoop
-    D; JEQ
-
-    @current_address
-    M=-1
-
-    @current_address
-    D=M+1
-    @current_address
-    M=D
-
-    @i
-    D=M-1
-    @i
-    M=D
-
-    @BlakenLoop
+    @LOOP2
     0; JMP
