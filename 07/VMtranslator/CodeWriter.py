e68279c6478ea4c3c8a525b935240c703a1c133b
Yuta Tezuka <yuta.tzk@gmail.com>
2022-12-08 22:22:13 +0900

    CodeWriterのインターフェースを記述

---
+++ 07/VMtranslator/CodeWriter.py
@@ -0,0 +1,12 @@
+class CodeWriter:
+    def __init__(self, path_to_file: str) -> None:
+        pass
+
+    def setFileName(self, filename: str) -> None:
+        pass
+
+    def writeArithmetic(self, command: str) -> None:
+        pass
+
+    def writePushPop(self, command: str, segment: str, index: int) -> None:
+        pass
