d91a44d49486257ef273cf86c281e7b16dc7e8f2
Yuta Tezuka <yuta.tzk@gmail.com>
2022-12-08 23:05:44 +0900

    コメントを追加

--- 07/VMtranslator/CommandType.py
+++ 07/VMtranslator/CommandType.py
@@ -3,7 +3,7 @@ class CommandType:
         self.c_arithmetic: str = "C_ARITHMETIC"
         self.c_push: str = "C_PUSH"
         self.c_pop: str = "C_POP"
-        self.c_label: str = "C_LABEL"
+        self.c_label: str = "C_LABEL" # 以下、この章では不要
         self.c_goto: str = "C_GOTO"
         self.c_if: str = "C_IF"
         self.c_function: str = "C_FUNCTION"
