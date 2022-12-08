4895acaa50f725e7b28a8fa4619a3c506d088b6b
Yuta Tezuka <yuta.tzk@gmail.com>
2022-12-08 22:18:40 +0900

    Parserのインターフェースを記述

---
+++ 07/VMtranslator/Parser.py
@@ -0,0 +1,18 @@
+class Parser:
+    def __init__(self, path_to_file: str) -> None:
+        pass
+
+    def hasMoreCommands(self) -> bool:
+        pass
+
+    def advance(self) -> None:
+        pass
+
+    def commandType(self) -> str:
+        pass
+
+    def arg1(self) -> str:
+        pass
+
+    def arg2(self) -> int:
+        pass
