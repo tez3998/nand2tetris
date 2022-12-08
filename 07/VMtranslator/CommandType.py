e3f6b9d9003b9265afa16b9f98ab5bc62ac5c165
Yuta Tezuka <yuta.tzk@gmail.com>
2022-12-08 22:42:18 +0900

    コマンドの種類を示すクラスを追加

---
+++ 07/VMtranslator/CommandType.py
@@ -0,0 +1,11 @@
+class CommandType:
+    def __init__(self) -> None:
+        self.c_arithmetic: str = "C_ARITHMETIC"
+        self.c_push: str = "C_PUSH"
+        self.c_pop: str = "C_POP"
+        self.c_label: str = "C_LABEL"
+        self.c_goto: str = "C_GOTO"
+        self.c_if: str = "C_IF"
+        self.c_function: str = "C_FUNCTION"
+        self.c_return: str = "C_RETURN"
+        self.c_call: str = "C_CALL"
