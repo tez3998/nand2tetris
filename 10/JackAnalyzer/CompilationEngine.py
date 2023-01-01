57fa477141883ff69a3e9dc5e69ee39030be6036
Yuta Tezuka <yuta.tzk@gmail.com>
2023-01-01 22:35:30 +0900

    仕様にあるインターフェースを用意

---
+++ 10/JackAnalyzer/CompilationEngine.py
@@ -0,0 +1,59 @@
+class CompilationEngine:
+    def __init__(self, path_to_output_file: str) -> None:
+        pass
+
+
+    def compile_class(self) -> None:
+        pass
+
+
+    def compile_class_var_dec(self) -> None:
+        pass
+
+
+    def compile_subroutine(self) -> None:
+        pass
+
+
+    def compile_parameter_list(self) -> None:
+        pass
+
+
+    def compile_var_dec(self) -> None:
+        pass
+
+
+    def compile_statements(self) -> None:
+        pass
+
+
+    def compile_do(self) -> None:
+        pass
+
+
+    def compile_let(self) -> None:
+        pass
+
+
+    def compile_while(self) -> None:
+        pass
+
+
+    def compile_return(self) -> None:
+        pass
+
+
+    def compile_if(self) -> None:
+        pass
+
+
+    def compile_expression(self) -> None:
+        pass
+
+
+    def compile_term(self) -> None:
+        pass
+
+
+    def compile_expression_list(self) -> None:
+        pass
---
+++ 10/JackAnalyzer/JackTokenizer.py
@@ -0,0 +1,35 @@
+class JackTokenizer:
+    def __init__(self, path_to_input_file: str) -> None:
+        pass
+
+
+    def has_more_tokens(self) -> bool:
+        pass
+
+
+    def advance(self) -> None:
+        pass
+
+
+    def token_type(self) -> int:
+        pass
+
+
+    def keyword(self) -> int:
+        pass
+
+
+    def symbol(self) -> str:
+        pass
+
+
+    def identifier(self) -> str:
+        pass
+
+
+    def int_val(self) -> int:
+        pass
+
+
+    def string_val(self) -> str:
+        pass
